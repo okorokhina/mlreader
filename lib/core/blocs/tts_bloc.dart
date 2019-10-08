e7aaf76dcfc520ab331d7438fe9cb2923e840fa2
chabelets <chabeletsv@gmail.com>
2019-10-08 13:52:41 +0300

    add tts

--- lib/core/blocs/tts_bloc.dart
+++ lib/core/blocs/tts_bloc.dart
@@ -1,3 +1,8 @@
-class TtsBloc{
+import 'package:mlreader/core/resourses/repository.dart';
 
+class TtsBloc{
+  Repository res = Repository();
+  getVoice(String voiceText){
+    res.getVoice(voiceText);
+  }
 }
